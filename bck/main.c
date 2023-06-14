fdf34fb24ccba1e7145051d874184171e9f08792
gianmarco-mameli <gnammyx@gmail.com>
2023-06-14 12:04:51 +0000

    working

--- bck/main.c
+++ bck/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "pico/stdlib.h"
+// #include "pico/stdlib.h"
 // #include "hardware/gpio.h"
 // #include "hardware/pwm.h"
 // #include "hardware/pio.h"
